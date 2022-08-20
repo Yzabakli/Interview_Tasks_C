707c52e1134b26d4e8922adeaa6b0073ca664483
Yzabakli <abakliyusufziya@gmail.com>
2022-08-20 14:32:35 +0200

    Fibonacci added

---
+++ number_tasks/Number_Fibonacci.c
@@ -0,0 +1,25 @@
+//
+// Created by Abakli on 20/08/2022.
+//
+
+
+#include <stdio.h>
+
+static int fibonacci(int number){
+
+    int j = 0;
+    int z = 1;
+
+    for (int i = 1; i < number; ++i){
+
+        z += j;
+
+        j = z - j;
+    }
+    return z;
+}
+
+int main(){
+
+    printf("%d", fibonacci(10));
+}
